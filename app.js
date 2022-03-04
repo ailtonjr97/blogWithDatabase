c32ba9c086989a305b63c60d10d7b9609d5a079f
Ailton de Souza Junior <ailton.souza97@hotmail.com>
2022-03-03 23:21:14 -0300

    Organized the code

--- app.js
+++ app.js
@@ -7,12 +7,10 @@ const ejs = require("ejs");
 const _ = require("lodash");
 
 const app = express();
-app.use(express.static("public"));
-
-mongoose.connect("mongodb+srv://ailtonjr://@cluster0.dq7kh.mongodb.net/articlesDB", { useNewUrlParser: true });
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+mongoose.connect("mongodb+srv://ailtonjr://@cluster0.dq7kh.mongodb.net/articlesDB", { useNewUrlParser: true });
 
 const articlesSchema = {
   title: String,
